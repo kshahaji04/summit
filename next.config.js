5a49653da9dc46c2d0326535581e1cc28a1cab5e
kshahaji04 <kshahaji04@gmail.com>
2024-08-05 13:31:08 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,20 @@
-/** @type {import('next').NextConfig} */
+// /** @type {import('next').NextConfig} */
 
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-  enabled: process.env.ANALYZE === 'true',
-});
-const nextConfig = {
-  reactStrictMode: false,
-  swcMinify: true,
-};
+// const withBundleAnalyzer = require('@next/bundle-analyzer')({
+//  enabled: process.env.ANALYZE === 'true',
+// });
+// const nextConfig = {
+//  reactStrictMode: false,
+//  swcMinify: true,
+// };
 
 // module.exports = withBundleAnalyzer(nextConfig);
+// module.exports = nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",  // <=== enables static exports
+  reactStrictMode: true,
+};
+
 module.exports = nextConfig;
