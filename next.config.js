1c56e06a92448caa1cbcfdd70ca52c51ac9e6fe2
kshahaji04 <kshahaji04@gmail.com>
2024-08-05 13:43:30 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -13,6 +13,9 @@
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  images: {
+    unoptimized: true,
+  },
   output: "export",  // <=== enables static exports
   reactStrictMode: true,
 };
